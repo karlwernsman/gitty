4a3f424725fd47ba8e61a67f878990f88a2ee4f4
karl wernsman <wernsmank@gmail.com>
2022-12-06 11:11:14 -0800

    POST to /api/v1/posts creates a new post for the signed in user

---
+++ __tests__/posts.test.js
@@ -0,0 +1,29 @@
+const setup = require('../data/setup.js');
+const request = require('supertest');
+const app = require('../lib/app.js');
+const pool = require('../lib/utils/pool.js');
+jest.mock('../lib/services/github');
+
+describe('posts', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('POST /api/v1/posts creates a new post for signed in user', async () => {
+    const agent = request.agent(app);
+    const user = await agent
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+    const newPost = { description: 'Howdy!', user_id: user.body.id };
+    const res = await agent.post('/api/v1/posts').send(newPost);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      description: newPost.description,
+      user_id: newPost.user_id,
+    });
+  });
+});
