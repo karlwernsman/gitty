e99eed62a45678491e9e6eb48311ebdbe1b508c2
karl wernsman <wernsmank@gmail.com>
2022-12-06 11:33:14 -0800

    /GET all posts passing

--- __tests__/posts.test.js
+++ __tests__/posts.test.js
@@ -2,6 +2,7 @@ const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
 const pool = require('../lib/utils/pool.js');
+const Post = require('../lib/models/Post.js');
 jest.mock('../lib/services/github');
 
 describe('posts', () => {
@@ -25,5 +26,18 @@ describe('posts', () => {
       description: newPost.description,
       user_id: newPost.user_id,
     });
+  });
+  it('GET /api/v1/posts should return a list of posts if logged in', async () => {
+    const agent = request.agent(app);
+    const user = await agent
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+    const newPost = await Post.insert({
+      description: 'Boyyy howdy',
+      user_id: user.body.id,
+    });
+    const res = await agent.get('/api/v1/posts');
+    expect(res.status).toBe(200);
+    expect(res.body[0]).toEqual(newPost);
   });
 });
