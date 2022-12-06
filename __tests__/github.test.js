a93d8617f845b51b3b5b681e4e313bdc6bb0f608
karl wernsman <wernsmank@gmail.com>
2022-12-05 16:55:33 -0800

    /api/v1/github/login test passing

---
+++ __tests__/github.test.js
@@ -0,0 +1,21 @@
+const pool = require('../lib/utils/pool.js');
+const setup = require('../data/setup.js');
+const request = require('supertest');
+const app = require('../lib/app.js');
+
+describe('github auth routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('/api/v1/github/login should redirect to the github oauth page', async () => {
+    const res = await request(app).get('/api/v1/github/login');
+    expect(res.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
+    );
+  });
+});
