695574395c25b9445c3c48e8df0e49e89267b0af
karl wernsman <wernsmank@gmail.com>
2022-12-05 17:34:46 -0800

    /api/v1/github/callback test passing

--- __tests__/github.test.js
+++ __tests__/github.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool.js');
 const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
+jest.mock('../lib/services/github');
 
 describe('github auth routes', () => {
   beforeEach(() => {
@@ -17,5 +18,20 @@ describe('github auth routes', () => {
     expect(res.header.location).toMatch(
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
     );
+  });
+  it('/api/v1/github/callback should login users and redirect to dashboard', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+    console.log('FLAGGGG', res.body);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      login: 'lottie_dog',
+      email: 'lottie@lottie.com',
+      avatar: 'https://www.placecage.com/gif/300/300',
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
   });
 });
